bc04520e25b1f660ff2770b29f17851643cccd74
Fatih Almutawakkil <fatihalmu@gmail.com>
2019-10-18 01:39:43 +0700

    feat/tugas1 - init

---
+++ Sibat/src/main/java/tugas1_1706043664/Sibat/SibatApplication.java
@@ -0,0 +1,13 @@
+package tugas1_1706043664.Sibat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibatApplication.class, args);
+	}
+
+}
